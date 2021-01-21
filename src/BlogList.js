0bae2afcaceca8703afed21469642e110420a3ab
DeeHuman <deeamezinapple@gmail.com>
2021-01-21 05:26:57 -0800

    fetch-data-from-server

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,5 +1,5 @@
 // just simple destructuring from props object
-const BlockList = ({blogs, title, handleDelete}) => {
+const BlockList = ({blogs, title}) => {
     return (
         <div className="blog-list">
             <h1>{title}</h1>
@@ -7,7 +7,6 @@ const BlockList = ({blogs, title, handleDelete}) => {
                 <div className="blog-preview" key={blog.id /* Key is important to react for performance issue */}>
                     <h2>{blog.title}</h2>
                     <p>Written By {blog.author}</p>
-                    <button onClick={()=> handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -4,37 +4,25 @@ import BlogList from "./BlogList";
 // sfc = stateless functional component
 // use keyword "sfc" to auto-generate template
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-
-    const handleDelete = id => {
-        const newBlogs = blogs.filter(blog=>blog.id!==id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(()=>{
-        console.log("React RENDERS! when blogs changes");
-    }, [blogs]);
+        fetch("http://localhost:8000/blogs")
+            .then(res => res.json())
+            .then(data => {
+                setBlogs(data);
+            })
+    }, []);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="Blogs" handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter(({author})=>author==="mario")} title="Mario's Blog" handleDelete={handleDelete}/>
+            {blogs && <BlogList blogs={blogs} title="Blogs"/>
+            // logical and: If expr1 can be converted to true, returns expr2; else, returns expr1.
+            }
         </div>
     );
 }
  
 export default Home;
 
-// useEffect is used to invoke a function when react renders
-// useState is active as it makes react rerenders while useEffect is passive as it run code when react renders
-// BEST FOR API
-
-/**
- * useEffect Dependency:
- * the second argument of useEffect is for Dependency (an array of useState constant, self-define variable gives no effect)
- * useEffect with Dependency only run at first render or when the useState constant changed by useState set function
- */
+// json server: npx json-server --watch data/db.json --port 8000
