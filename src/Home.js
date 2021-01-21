6e78a873cb98bb1183c4794c627dc5dc422050f8
DeeHuman <deeamezinapple@gmail.com>
2021-01-21 04:08:42 -0800

    outputting-list(learn-about-keys)

--- src/Home.js
+++ src/Home.js
@@ -3,22 +3,28 @@ import { useState } from "react";
 // sfc = stateless functional component
 // use keyword "sfc" to auto-generate template
 const Home = () => {
-    const [name, setName] = useState("mario");
-    const [age, setAge] = useState(25);
-
-    const handleClick = (e) => {
-        setName("Rap...per");
-        setAge(10009);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>Your Love One: {name}</p>
-            <p>Age: {age}</p>
-            <button onClick={handleClick}>Click Me</button>
+            {
+                blogs.map(blog=>(
+                    <div className="blog-preview" key={blog.id /* Key is important to react for performance issue */}>
+                        <h2>{blog.title}</h2>
+                        <p>Written By {blog.author}</p>
+                    </div>
+                ))
+            }
         </div>
     );
 }
  
 export default Home;
+
+// Key is used to compare the previous state of the DOM and current state of the DOM (after changes).
+// if key is absent, react will just do DUMB shit that effect performance instead of doing some smart moves
+// react docs: https://reactjs.org/docs/reconciliation.html#recursing-on-children
