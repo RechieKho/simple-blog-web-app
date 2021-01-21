a5e5f229a26a3a49a521317cbaa41aec589fa8d4
DeeHuman <deeamezinapple@gmail.com>
2021-01-20 21:54:42 -0800

    dynamic-values-in-templates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Hello from the other side";
+  const loves = 3000;
+  const site = "https://www.google.com"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1> 
+        <p>Love you {loves}</p>
+
+        <p>Random Number : {Math.random()* 10}</p>
+        <a href={site}>Google Site</a>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+/**
+ * Notes: 
+ * 1. We can't output boolean and object through render (Array is possible, React will just dump the square bracket and comma)
+ */
