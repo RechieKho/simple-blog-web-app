182649f20ec7ef86547a9285a44d0ea932d2ba6c
DeeHuman <deeamezinapple@gmail.com>
2021-01-20 23:46:04 -0800

    DevTool

--- src/Home.js
+++ src/Home.js
@@ -22,20 +22,3 @@ const Home = () => {
 }
  
 export default Home;
-
-// useState tells react to watch over the data so that the UI will update when the data changes
-/**
- * To use state: 
- * const [a const variable, set Function] = useState(DEFAULT_VALUE)
- * 
- * you know... useState return an array of a constant and a function and we just immediately destructure it.
- * 
- * At first I think the const variable should be just a get function so that it PAIRS (GetFunciton, SetFunction, how nice)
- * But a const variable is way more cheaper than a function that return values in term of memory
- * 
- * To set value base on the previous value:
- * setFunction(prevValue => {DO SOMETHING})
- * 
- * This is because that the const variable IS just a const variable. The const variable only changes when react rerun the whole thing and react
- * will not rerun the whole thing when the function that invoke the set function is being invoked
- */
