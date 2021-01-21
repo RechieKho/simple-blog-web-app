1bdd6e8156345634273d86c05d36291710c45da2
DeeHuman <deeamezinapple@gmail.com>
2021-01-20 22:49:16 -0800

    adding-styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,10 +6,18 @@ const Navbar = () => {
             <h1>The Dojo Block</h1>
             <div className="link">
                 <a href="/">Home</a>
-                <a href="/create">New Block</a>
+                <a href="/create" style={{
+                    color: 'white', 
+                    backgroundColor: '#f1356d', 
+                    borderRadius: '8px'
+                }}>New Block</a>
             </div>
         </nav>
     );
 }
  
 export default Navbar;
+
+/**
+ * when using dynamic inline style, object is used instead of string
+ */
