448995d29d55039024fd481b92b787261a8109a0
DeeHuman <deeamezinapple@gmail.com>
2021-01-22 00:58:38 -0800

    Router-param

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import {Link} from 'react-router-dom';
+
 // just simple destructuring from props object
 const BlockList = ({blogs, title}) => {
     return (
@@ -5,8 +7,10 @@ const BlockList = ({blogs, title}) => {
             <h1>{title}</h1>
             {blogs.map(blog=>(
                 <div className="blog-preview" key={blog.id /* Key is important to react for performance issue */}>
-                    <h2>{blog.title}</h2>
-                    <p>Written By {blog.author}</p>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{blog.title}</h2>
+                        <p>Written By {blog.author}</p>
+                    </Link>
                 </div>
             ))}
         </div>
