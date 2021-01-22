54b9050a01d8d3b88ea9749e29685a4b94253bd1
DeeHuman <deeamezinapple@gmail.com>
2021-01-21 20:09:43 -0800

    react-router-link

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 // sfc = stateless functional component
 // use keyword "sfc" to auto-generate template
 const Navbar = () => {
@@ -5,12 +7,12 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>The Dojo Block</h1>
             <div className="link">
-                <a href="/">Home</a>
-                <a href="/create" style={{
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
                     color: 'white', 
                     backgroundColor: '#f1356d', 
                     borderRadius: '8px'
-                }}>New Block</a>
+                }}>New Block</Link>
             </div>
         </nav>
     );
