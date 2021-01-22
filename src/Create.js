11f67fc3879cd21a7ef83b13467cdeb6de80e370
DeeHuman <deeamezinapple@gmail.com>
2021-01-22 04:06:16 -0800

    control-input

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
 const Create = () => {
-    return ( <div>
-        <h1>Add New Blog</h1>
-    </div> );
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("mario");
+
+    return ( 
+        <div className="create">
+            <form>
+                <label>Blog Title: </label>
+                <input 
+                    required
+                    type="text"
+                    value={title}
+                    onChange={e=>setTitle(e.target.value)}
+                />
+                <label>Blog Body: </label>
+                <textarea
+                    required
+                    type="text"
+                    value={body}
+                    onChange={e=>setBody(e.target.value)}
+                />
+                <label>Blog Author: </label>
+                <select
+                    value={author}
+                    onChange={e=>setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
+        </div>
+    );
 }
  
 export default Create;
